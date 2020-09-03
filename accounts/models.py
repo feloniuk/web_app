6f7cef39d6fc7f016274c7397f40e6156384a898
Feloniuk <s-feloniuk@ukr.net>
2020-09-03 20:44:45 +0300

    homework #5 model Publication

--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,20 @@ class Profile(models.Model):
         return f'nickname - {self.nickname}, id - {self.id}, password - {self.password}'
 
 
+class Publication(models.Model):
+    author = models.ManyToManyField(Profile)
+    description = models.CharField(max_length=255)
+    release_date = models.DateTimeField(auto_now_add=True)
+    publication = models.FileField(upload_to='media/')
+
+    def __str__(self):
+        return f'author - {self.author}'
+
+
+class Comment(models.Model):
+    author = models.ManyToManyField(Profile)
+    comment = models.CharField(max_length=255)
+    release_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'author - {self.author}'
