1f90d7e348033030d7043bbd486118a0933f3df7
Feloniuk <s-feloniuk@ukr.net>
2020-09-11 11:33:55 +0300

    #11

--- src/app/urls.py
+++ src/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from accounts import views
 from app import settings
 
@@ -23,10 +23,18 @@ app_name = 'app'
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('publications/', views.get_publications_list),
-                  path('profiles/', views.ProfilesListView.as_view()),
-                  path('profiles/add/', views.ProfileCreateView.as_view()),
-                  path('profiles/show/<slug>', views.get_profile),
-                  path('profiles/edit/<slug>', views.edit_profile),
-                  path('', views.get_publications_list, name='home')
+                  path('profiles/', include('accounts.urls')),
+                  path('', include('accounts.urls')),
+                  path('publications/', include('publications.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#[
+#    path('admin/', admin.site.urls),
+#    path('publications/', views.get_publications_list),
+#    path('profiles/', views.ProfilesListView.as_view()),
+#    path('profiles/add/', views.ProfileCreateView.as_view()),
+#    path('profiles/show/<slug>', views.get_profile),
+#    path('profiles/edit/<slug>', views.edit_profile),
+#    path('', views.get_publications_list, name='home')
+#]
