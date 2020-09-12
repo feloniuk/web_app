2d25842c26972b9dad22d71115ba857466712c65
Feloniuk <s-feloniuk@ukr.net>
2020-09-12 16:27:09 +0300

    #13

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import get_profiles_list, get_profile, add_profile
+from accounts.views import get_profiles_list, get_profile, add_profile, edit_profile
 
 app_name = 'accounts'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', get_profiles_list, name='list'),
     path('add/', add_profile, name='add'),
     path('show/<id>', get_profile, name='show'),
+    path('profiles/show/<slug>', get_profile),
+    path('profiles/edit/<slug>', edit_profile),
 ]
--- src/publications/views.py
+++ src/publications/views.py
@@ -1,3 +1,29 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from accounts.forms import PublicationEditForm
+from accounts.models import Publication
+
+
+def edit_publication(request, slug):
+    try:
+        profile = Publication.objects.get(id=slug)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Profile id {slug} doesnt exist')
+    if request.method == 'POST':
+        form = PublicationEditForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f'/profiles/show/{slug}')
+
+    elif request.method == 'GET':
+        form = PublicationEditForm(instance=profile)
+
+    return render(
+        request,
+        template_name='profile_edit.html',
+        context={
+            'form': form
+        }
+    )
