8823555b9f1894d60bc78caa7919625b5e342c57
Feloniuk <s-feloniuk@ukr.net>
2020-09-23 12:35:40 +0300

    #15

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
 ENV PYTHONUNBUFFERED 1
-RUN skdir /zeroten_skaner
+RUN mkdir /zeroten_skaner
 WORKDIR /zeroten_skaner
 ADD requirements.txt /zeroten_skaner/
 RUN pip install -r requirements.txt
--- src/app/settings.py
+++ src/app/settings.py
@@ -75,12 +75,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '19971816',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
     }
 }
 
