3b27c690ffee035e0a89171a051b9ff0432f0d37
Feloniuk <s-feloniuk@ukr.net>
2020-10-13 19:44:40 +0300

    #16 - Custom Tests

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from faker import Faker
+
+from accounts.models import Profile
+
+f = Faker()
+
 
 # Create your tests here.
+
+class AccountsTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_create_user(self):
+        email = f.email()
+        user = User.objects.create_user(
+            username=f.user_name(),
+            email=f.email(),
+            password=f.password()
+        )
+        profile = Profile.object.get(user_id=user.id)
+        self.assertEqual(profile.nickname, email)
+
+    def tearDown(self) -> None:
+        pass
